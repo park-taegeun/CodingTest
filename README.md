1a4dd1a7640d1c49603cdeda69cd555ac336bed2
박태근 <taegeun1234@naver.com>
2026-07-10 11:23:50 +0900

    릿코드 저장소 초기 세팅

---
+++ README.md
@@ -0,0 +1,6 @@
+# CodingTest (LeetCode)
+
+파이썬으로 푸는 릿코드 풀이 저장소.
+
+구조: easy / medium / hard 난이도별 풀이
+파일명 규칙: 문제번호_문제이름.py (예: 001_two_sum.py)
---
+++ template.py
@@ -0,0 +1,9 @@
+from typing import List, Optional
+
+class Solution:
+    def solve(self):
+        pass
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.solve())
